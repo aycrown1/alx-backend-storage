b55beb004a1d639bb8d9f291a9d1bff747a4f6d4
Ayoade Adekola <108250568+aycrown1@users.noreply.github.com>
2024-01-25 02:51:34 +0100

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -38,6 +38,29 @@ def call_history(method: Callable) -> Callable:
     return invoker
 
 
+def replay(fn: Callable) -> None:
+"""
+display the history of calls of a particular function
+"""
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        decoded_input = fxn_input.decode("utf-8")
+        decoded_output = fxn_output.decode("utf-8") if fxn_output else "None"
+        print('{}(*{}) -> {}'.format(fxn_name, decoded_input, decoded_output))
+
 
 class Cache:
     """Represents an object for storing data in a Redis data storage.
