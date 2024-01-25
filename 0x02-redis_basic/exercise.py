f09855ac2a2dd83d5501c57a95ce6c51f9c04fa1
Ayoade Adekola <108250568+aycrown1@users.noreply.github.com>
2024-01-25 02:38:08 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -62,3 +62,23 @@ class Cache:
         """Generates a random key using uuid.
         """
         return str(uuid.uuid4())
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """Retrieves a value from a Redis data storage.
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value from a Redis data storage.
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from a Redis data storage.
+        """
+        return self.get(key, lambda x: int(x))
